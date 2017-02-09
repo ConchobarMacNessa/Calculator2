0315eb79a4a69bed68c880bd132fac3453f21b88
Alice BC <alice.binghamcarr@gmail.com>
2017-02-09 11:15:43 +0000

    add clear functions

--- scripts.js
+++ scripts.js
@@ -39,3 +39,37 @@ function exponent(){ //to handle really big numbers
 		input.removeChild(input.firstChild);
 		input.appendChild(document.createTextNode(current));
 }
+
+// function negativeOrPos() { //makes the number negative if you press + and - //THIS NEEDS SORTING OUT
+// 		if (current.indexOf('e') != -1){ //if there is an exponent
+// 			var exponent = current.indexOf('e-');
+// 			if (exponent != -1){
+// 				current = current.substring(0, 1 + exponent) + current.substring(2 + exponent);
+// 			} else { 
+// 				exponent = current.indexOf('e');
+// 				current = current.substring(0, 1 + exponent) + '-' + current.substring(1 + exponent);
+// 			}
+// 		} else { // ie if there is no exponent
+// 			if ( current.indexOf('-') == 0){
+// 				current = current.substring(1);
+// 			} else {
+// 				current = '-' + current;
+// 			}
+// 		}
+// }
+
+function clearLast(){
+	current = '0';
+		while (input.firstChild)
+		input.removeChild(input.firstChild);
+		input.appendChild(document.createTextNode(current));
+}
+
+function clearAll(){
+	current = '0';
+	memory = '0';
+	operation = 0;
+	while (input.firstChild)
+	input.removeChild(input.firstChild);
+	input.appendChild(document.createTextNode(current));
+}
